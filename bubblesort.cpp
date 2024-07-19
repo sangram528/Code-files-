6151cb4fb355725f8632c4eb694855248b5f5169
Sangram <lordmetal1803@gmail.com>
2024-07-19 10:50:10 +0530

    fourthcommit

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -6,11 +6,19 @@ void ascendingsort(int array[], int size);
 void descendingsort(int array[], int size);
 int main(){
 
-    int array[] = {5,6,8,10,12,54,87,100,36,56,99,8};
+    int array[5];
     int size = sizeof(array)/sizeof(array[0]);
-
+    std::cout<<"Enter the elements of array: \n";
+    for(int i=0;i<size;i++){
+        std::cout<<"Index "<<i<<":\n";
+        std::cin>>array[i];
+    }
+    std::cout<<"\nOriginal Array: \n";
+    for(int j:array){
+        std::cout<< j<< " ";
+    }
     ascendingsort(array,size);
-    std::cout<<"Array sort in ascending order \n";
+    std::cout<<"\nArray sort in ascending order \n";
     for(int element:array){
         std::cout<< element<< " ";
     }
